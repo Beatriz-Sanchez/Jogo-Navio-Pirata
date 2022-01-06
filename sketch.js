225f4bb14b0216916b790fc7c95b3d59e56031f8
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-01-06 11:39:31 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+
 var engine, world;
 var canvas;
+var ground;
 
 function preload() {
  
@@ -14,10 +16,19 @@ function setup() {
   
   engine = Engine.create();
   world = engine.world;
+
+  var ground_options = {
+    isStatic: true
+  };
+  ground = Bodies.rectangle(0,height-1,width*2,1,ground_options);
+  World.add(world,ground);
 }
 
 function draw() {
   background(189);
 
   Engine.update(engine);
+
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,width*2,1);
 }
