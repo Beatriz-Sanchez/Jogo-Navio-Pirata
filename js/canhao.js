89c2225a4f830d51d1d8fbe5591326ca7a106159
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-01-06 12:53:41 -0300

    Update and rename cannon.js to canhao.js

---
+++ js/canhao.js
@@ -0,0 +1,10 @@
+class Canhao {
+  constructor(x,y,largura,altura,angulo){
+    this.x = x;
+    this.y = y;
+    this. largura = largura;
+    this.altura = altura;
+    this.angulo = angulo;
+  }
+  
+}
