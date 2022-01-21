36cc9f9a13c7f1d2fdbd9891d30d8c7ed5b8bc2f
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-01-21 11:49:06 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,10 @@ var engine, world;
 var canvas;
 var fundoImg, torreImg;
 var solo, torre, canhao, bala;
+//angulo inicial do canhão
 var angulo = 20;
+//matriz das balas
+var balas = [];
 
 function preload() {
   fundoImg = loadImage("./assets/background.gif");
@@ -20,17 +23,16 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
+  //criando solo e torre
   var options = {
     isStatic: true
   };
   solo = Bodies.rectangle(0,height-1,width*2,1,options);
   World.add(world,solo);
-
   torre = Bodies.rectangle(160,350,160,310,options);
   World.add(world,torre);
 
   canhao = new Canhao(180,110,130,100,angulo);
-  bala = new Bala(canhao.x,canhao.y);
 
   angleMode(DEGREES);
 }
@@ -40,6 +42,7 @@ function draw() {
 
   Engine.update(engine);
 
+  //desenhando o solo
   rect(solo.position.x,solo.position.y,width*2,1);
 
   push();
@@ -47,11 +50,32 @@ function draw() {
   image(torreImg,torre.position.x,torre.position.y,160,310);
   pop();
 
+  //mostrando as balas de canhão
+  for (var i = 0; i < balas.length; i++) {
+    mostrarBala(balas[i]);
+  }
+
   canhao.display();
-  bala.display();
 }
+
+//se a tecla para baixo for apertada, uma bola é criada
+function keyPressed(){
+  if (keyCode === DOWN_ARROW){
+    var bala = new Bala(canhao.x, canhao.y+4);
+    balas.push(bala);
+  }
+}
+
+//se a tecla para baixo for solta uma bola é atirada
 function keyReleased(){
   if (keyCode === DOWN_ARROW){
-    bala.atirar();
+    balas[balas.length - 1].atirar();
+  }
+}
+
+//função para mostrar cada bala
+function mostrarBala(bala){
+  if(bala){
+      bala.display();
   }
 }
